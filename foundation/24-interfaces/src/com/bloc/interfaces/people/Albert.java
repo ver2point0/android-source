869dc9f73ffa5606b9efbbcf137d77aa7702da3c
John McIntosh II <jmci10@hotmail.com>
2015-05-22 15:06:39 -0400

    Checkpoint 24 completed

--- foundation/24-interfaces/src/com/bloc/interfaces/people/Albert.java
+++ foundation/24-interfaces/src/com/bloc/interfaces/people/Albert.java
@@ -1,12 +1,29 @@
 package com.bloc.interfaces.people;
 
+import com.bloc.interfaces.people.hobbies.SalsaDancer;
+
 /************************************************
  *	ASSIGNMENT:
  *	Have Albert implement the SalsaDancer interface
 /************************************************/
 
-public class Albert extends Person {
+public class Albert extends Person implements SalsaDancer {
 	public Albert() {
 		super("Albert", "Cobb", "Male", 1.8d, 72d, "Green");
+	}
+
+	@Override
+	public void putOnShoes() {
+
+	}
+
+	@Override
+	public void findAPartner() {
+
+	}
+
+	@Override
+	public void salsa() {
+
 	}
 }
