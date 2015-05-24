6de78d07de1365da76aafb3fcb911e9ba3752d39
John McIntosh II <jmci10@hotmail.com>
2015-05-24 17:49:37 -0400

    Checkpoint 31 completed

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,6 +1,15 @@
 package com.bloc.threads;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
+
+	private URL mURL;
+	private boolean mOpenWhenCompleted;
 
 	/*
 	 * ImageGetter
@@ -13,18 +22,49 @@ public class ImageGetter extends Thread {
 	 *		  as soon it is downloaded, `false` otherwise
 	 */
 	/************************************************
- 	 *	ASSIGNMENT:
- 	 *	Implement this constructor for ImageGetter.
- 	 *	The variables passed into it must impact the `run()` method's
- 	 *	behavior as described above.
-	/************************************************/
+	 *	ASSIGNMENT:
+	 *	Implement this constructor for ImageGetter.
+	 *	The variables passed into it must impact the `run()` method's
+	 *	behavior as described above.
+	 /************************************************/
+	public ImageGetter(String url, boolean openWhenCompleted){
+		try{
+			mURL = new URL(url);
+			mOpenWhenCompleted = openWhenCompleted;
+		}
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	@Override
 	public void run() {
 		/************************************************
- 		 *	ASSIGNMENT:
- 		 *	Perform the work found in `Main.java` in this
- 		 *	Thread instead.
-		/************************************************/
+		 *	ASSIGNMENT:
+		 *	Perform the work found in `Main.java` in this
+		 *	Thread instead.
+		 /************************************************/
+		try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
+			BufferedImage bufferedImage = ImageIO.read(url);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
--- foundation/31-threads/src/com/bloc/threads/Main.java
+++ foundation/31-threads/src/com/bloc/threads/Main.java
@@ -13,35 +13,13 @@ public class Main extends Object {
 		 *	ASSIGNMENT:
 		 *	Perform the following block of code on a separate
 		 *	Thread, use the ImageGetter class.
-		/************************************************/
-
-		try {
-			File existingImage = new File("google_logo.png");
-			if (existingImage.exists()) {
-				existingImage.delete();
-			}
-			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
-			BufferedImage bufferedImage = ImageIO.read(url);
-			File outputfile = new File("google_logo.png");
-			ImageIO.write(bufferedImage, "png", outputfile);
-			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
-				Runtime.getRuntime().exec("open google_logo.png");
-			} else {
-				Runtime.getRuntime().exec("google_logo.png");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		 /************************************************/
 
 		/************************************************
 		 *	ASSIGNMENT:
 		 *	Perform the previous block of code on a separate
 		 *	Thread, use the ImageGetter class.
-		/************************************************/
+		 /************************************************/
 
 		File logo = new File("google_logo.png");
 		boolean exists = false;
